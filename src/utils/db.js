7f8becaecab51ae00915c5871aa6be63aec006af
ricardomiranda-allims <54359457+ricardomiranda-allims@users.noreply.github.com>
2022-10-14 12:59:39 -0300

    ajustando conexão via ssl

--- src/utils/db.js
+++ src/utils/db.js
@@ -5,19 +5,33 @@ const { Client } = require('pg')
 const getRows = async (sql, params = []) => {
   let data = []
   try {
-    const { PGHOST, PGPORT, PGDATABASE, PGUSER, PGPASSWORD } = process.env
+    const {
+      PGHOST = 'localhost',
+      PGPORT = 5432,
+      PGDATABASE = 'postgres',
+      PGUSER = 'postgres',
+      PGPASSWORD = 'postgres',
+      PGSSL = false
+    } = process.env
     const client = new Client({
       host: PGHOST,
       port: PGPORT,
       database: PGDATABASE,
       user: PGUSER,
-      password: PGPASSWORD
+      password: PGPASSWORD,
+      ssl: toBoolean(PGSSL)
     })
     await client.connect()
     const res = (await client.query(sql, params)) || {}
     await client.end()
     data = res.rows || []
-  } catch {}
+  } catch (error) {
+    console.error(error)
+  }
   return data
 }
 exports.getRows = getRows
+
+const toBoolean = value =>
+  (typeof value === 'boolean' && value) ||
+  (typeof value === 'string' && value.toLowerCase().trim() === 'true')
